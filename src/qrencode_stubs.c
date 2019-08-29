05ba1a3bd8f9fa4bf093545787109378656a843d
Vincent Bernardoff <vb@luminar.eu.org>
2019-08-29 19:42:33 +0200

    style

--- src/qrencode_stubs.c
+++ src/qrencode_stubs.c
@@ -1,22 +1,13 @@
-/* 
- * stubs
- */
-
-// STD
 #include <stdio.h>
 #include <string.h>
 
-// PNG
-#include <png.h>
-
-// OCAML 
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
 #include <caml/custom.h>
 
-// QRENCODE 
 #include <qrencode.h>
+#include <png.h>
 
 /********************************/
 /* Dealing with 'struct QRcode' */
@@ -36,8 +27,7 @@ static struct custom_operations qrcode_ops = {
 #define QRcode_val(v) (*((QRcode **) Data_custom_val(v)))
 
 /* Allocating a Caml custom block to hold the given QRcode * */
-static value alloc_QRcode(QRcode * w)
-{
+static value alloc_QRcode(QRcode * w) {
   value v = alloc_custom(&qrcode_ops, sizeof(QRcode *), 0, 1);
   QRcode_val(v) = w;
   return v;
@@ -51,10 +41,9 @@ static value alloc_QRcode(QRcode * w)
 /* Accessing the QRinput * part of a Caml custom block */
 #define QRinput_val(v) (*((QRinput **) Data_custom_val(v)))
 
-void ocaml_QRinput_finalize (value qrinput) 
-{
-  printf ("@@@ About to finalize qrinput value"); 
-  QRinput_free (QRinput_val (qrinput)); 
+void ocaml_QRinput_finalize (value qrinput) {
+  printf ("@@@ About to finalize qrinput value");
+  QRinput_free (QRinput_val (qrinput));
   return ;
 }
 
@@ -65,12 +54,11 @@ static struct custom_operations qrinput_ops = {
   custom_hash_default,
   custom_serialize_default,
   custom_deserialize_default,
-  custom_compare_ext_default 
+  custom_compare_ext_default
 };
 
 /* Allocating a Caml custom block to hold the given QRinput * */
-static value alloc_QRinput(QRinput * w)
-{
+static value alloc_QRinput(QRinput * w) {
   value v = alloc_custom(&qrinput_ops, sizeof(QRinput *), 0, 1);
   QRinput_val(v) = w;
   return v;
@@ -83,11 +71,10 @@ static value alloc_QRinput(QRinput * w)
 /* Accessing the QRencodeMode * part of a Caml custom block */
 #define QRencodeMode_val(v) (*((QRencodeMode **) Data_custom_val(v)))
 
-void ocaml_QRencodeMode_finalize (value mode)
-{
-  printf ("@@@ About to finalize mode value"); 
-  free (QRencodeMode_val (mode)) ; 
-  return ;  
+void ocaml_QRencodeMode_finalize (value mode) {
+  printf ("@@@ About to finalize mode value");
+  free (QRencodeMode_val (mode)) ;
+  return ;
 }
 
 static struct custom_operations qrencodemode_ops = {
@@ -108,80 +95,68 @@ static value alloc_QRencodeMode(QRencodeMode * w)
   return v;
 }
 
-
 /***********************/
 /* Cst creation        */
 /* This is probably overkill */
 /***********************/
 
-value ocaml_QRencodeMode_num (value unit) 
-{
-  CAMLparam1(unit); 
-  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode)); 
-  *mode = QR_MODE_NUM; 
+value ocaml_QRencodeMode_num (value unit) {
+  CAMLparam1(unit);
+  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode));
+  *mode = QR_MODE_NUM;
   CAMLreturn (alloc_QRencodeMode(mode)) ;
 }
 
-value ocaml_QRencodeMode_an(value unit) 
-{
-  CAMLparam1(unit); 
-  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode)); 
-  *mode = QR_MODE_AN; 
+value ocaml_QRencodeMode_an(value unit) {
+  CAMLparam1(unit);
+  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode));
+  *mode = QR_MODE_AN;
   CAMLreturn (alloc_QRencodeMode(mode)) ;
 }
 
-value ocaml_QRencodeMode_8 (value unit) 
-{
-  CAMLparam1(unit); 
-  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode)); 
-  *mode = QR_MODE_8; 
+value ocaml_QRencodeMode_8 (value unit) {
+  CAMLparam1(unit);
+  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode));
+  *mode = QR_MODE_8;
   CAMLreturn (alloc_QRencodeMode(mode)) ;
 }
 
-value ocaml_QRencodeMode_kanji (value unit) 
-{
-  CAMLparam1(unit); 
-  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode)); 
-  *mode = QR_MODE_KANJI; 
+value ocaml_QRencodeMode_kanji (value unit) {
+  CAMLparam1(unit);
+  QRencodeMode *mode = (QRencodeMode *) malloc (sizeof (QRencodeMode));
+  *mode = QR_MODE_KANJI;
   CAMLreturn (alloc_QRencodeMode(mode)) ;
 }
-
-
 
 /**********************************/
 /* Input data                     */
 /**********************************/
 
-value ocaml_QRinput_new(value unit)
-{
+value ocaml_QRinput_new(value unit) {
   CAMLparam1 (unit);
-  QRinput * new_input = QRinput_new (); 
+  QRinput * new_input = QRinput_new ();
   CAMLreturn (alloc_QRinput (new_input));
 }
 
-
-value ocaml_QRinput_append(value input, value mode, value data)
-{
+value ocaml_QRinput_append(value input, value mode, value data) {
   CAMLparam3 (input, mode, data);
 
   int rc = QRinput_append (QRinput_val (input),
-			   *(QRencodeMode_val(mode)),
-			   caml_string_length(data),
-			   (unsigned char*)String_val(data));
+                           *(QRencodeMode_val(mode)),
+                           caml_string_length(data),
+                           (unsigned char*)String_val(data));
 
   CAMLreturn (Val_int(rc));
 }
-
 
 /***********************************/
 /* Code data                       */
 /***********************************/
 
-value ocaml_QRcode_encodeInput (value input) 
-{
-  CAMLparam1(input); 
-  QRcode * new_code = QRcode_encodeInput (QRinput_val (input)); 
-  CAMLreturn (alloc_QRcode (new_code)); 
+value ocaml_QRcode_encodeInput (value input) {
+  CAMLparam1(input);
+  QRcode * new_code = QRcode_encodeInput (QRinput_val (input));
+  CAMLreturn (alloc_QRcode (new_code));
 }
 
 
@@ -189,9 +164,7 @@ value ocaml_QRcode_encodeInput (value input)
 /* Write PNG                       */
 /***********************************/
 
-
-static int writePNG(QRcode *qrcode, int size, int margin, const char *outfile)
-{
+static int writePNG(QRcode *qrcode, int size, int margin, const char *outfile) {
   static FILE *fp; // avoid clobbering by setjmp.
   png_structp png_ptr;
   png_infop info_ptr;
@@ -237,12 +210,12 @@ static int writePNG(QRcode *qrcode, int size, int margin, const char *outfile)
 
   png_init_io(png_ptr, fp);
   png_set_IHDR(png_ptr, info_ptr,
-	       realwidth, realwidth,
-	       1,
-	       PNG_COLOR_TYPE_GRAY,
-	       PNG_INTERLACE_NONE,
-	       PNG_COMPRESSION_TYPE_DEFAULT,
-	       PNG_FILTER_TYPE_DEFAULT);
+               realwidth, realwidth,
+               1,
+               PNG_COLOR_TYPE_GRAY,
+               PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_DEFAULT,
+               PNG_FILTER_TYPE_DEFAULT);
   png_write_info(png_ptr, info_ptr);
 
   /* top margin */
@@ -261,12 +234,12 @@ static int writePNG(QRcode *qrcode, int size, int margin, const char *outfile)
     bit = 7 - (margin * size % 8);
     for(x=0; x<qrcode->width; x++) {
       for(xx=0; xx<size; xx++) {
-	*q ^= (*p & 1) << bit;
-	bit--;
-	if(bit < 0) {
-	  q++;
-	  bit = 7;
-	}
+        *q ^= (*p & 1) << bit;
+        bit--;
+        if(bit < 0) {
+          q++;
+          bit = 7;
+        }
       }
       p++;
     }
@@ -289,10 +262,8 @@ static int writePNG(QRcode *qrcode, int size, int margin, const char *outfile)
   return 0;
 }
 
-
-value ocaml_writePNG (value code, value size, value margin, value outfile) 
-{
-  CAMLparam2 (code, outfile); 
-  writePNG(QRcode_val (code), Int_val(size), Int_val (margin), String_val (outfile)); 
-  CAMLreturn (Val_unit); 
+value ocaml_writePNG (value code, value size, value margin, value outfile) {
+  CAMLparam2 (code, outfile);
+  writePNG(QRcode_val (code), Int_val(size), Int_val (margin), String_val (outfile));
+  CAMLreturn (Val_unit);
 }
