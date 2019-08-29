60bbab3e1a71856634a478f2813d53d63adcb71f
Vincent Bernardoff <vb@luminar.eu.org>
2019-08-29 19:38:33 +0200

    switch to dune

--- test/test.ml
+++ test/test.ml
@@ -1,10 +1,8 @@
-open Printf
-open QRencode
+open Qrencode
 
 let _ =
   let input = QRinput.create () in
   let mode = QRencodeMode.mode_8 () in
-  let rc = QRinput.append input mode "http://www.corefarm.com" in
+  let _rc = QRinput.append input mode "http://www.tezos.com" in
   let code = QRcode.encode input in
-   QRcode.to_png code 10 4 "/tmp/out.png"
-
+  QRcode.to_png code 10 4 "/tmp/out.png"
